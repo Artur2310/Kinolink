6971067b3e1b11b52ae02e750308c3159c5213ca
Artur <Paradiz2310@gmail.com>
2019-06-06 16:04:19 +0300

    Add remote schema database

--- kinolink-web/src/main/java/ru/kinolink/web/webapp/config/DatabaseConfig.java
+++ kinolink-web/src/main/java/ru/kinolink/web/webapp/config/DatabaseConfig.java
@@ -1,15 +1,22 @@
 package ru.kinolink.web.webapp.config;
 
+import com.google.common.io.ByteStreams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import ru.kinolink.service.config.ServiceConfig;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Import(ServiceConfig.class)
 public class DatabaseConfig {
@@ -17,28 +24,8 @@ public class DatabaseConfig {
     @Autowired
     Environment environment;
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
-        basicDataSource.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
-        basicDataSource.setUsername("root");
-        basicDataSource.setPassword("Klarkson2310!");
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/kinolink?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-
-        /*try {
-            InputStream inputStream = new URL("https://downloader.disk.yandex.ru/disk/1a4398c3d66caca20caaf209561b5be9cb1828687b1e28eccc4170c332dc8ca4/5cf91ec6/8Vx--aj7_7HSb0_BTSbtHiLAKBPjj2VZRMnuB5--6d1huO_bkISLzR-BsYFkNH6ATY2k4jr-hcPMzHkoD1nKKg%3D%3D?uid=0&filename=data.sql&disposition=attachment&hash=b6SgPJL%2BieKbO5SoKUGuvcI3a2FjTV/RI6mx5JabRgGPbdMemoMElXCZv62Z1Ry/q/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=text%2Fplain&fsize=28710680&hid=a2d8c24900aaa4de8b9fedfc1003b642&media_type=data&tknv=v2").openStream();
-            if (environment.getProperty("init_data").equals("true")) {
-
-                Resource initSchema = new FileUrlResource("C:\\kinolink\\movie.sql");
-                //Resource initSchema = new ByteArrayResource(IOUtils.readNBytes(inputStream, inputStream.available()));
-                DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initSchema);
-                DatabasePopulatorUtils.execute(databasePopulator, basicDataSource);
-            }
-        }catch (Exception e){
-
-        }*/
-        return basicDataSource;
-    }
+    @Autowired
+    DataSource dataSource;
 
     /**
      * Init schema and data
@@ -47,15 +34,22 @@ public class DatabaseConfig {
     public DataSourceInitializer dataSourceInitializer() {
 
         ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        try{
-            //resourceDatabasePopulator.addScript(new ClassPathResource("schema.sql"));
-            //resourceDatabasePopulator.addScript(new FileUrlResource("C:\\kinolink\\data.sql"));
-    }catch (Exception e){
+        try {
+            if (environment.getProperty("init_data").equals("true")) {
+                InputStream inputStream = new URL(environment.getProperty("url_data")).openStream();
+                Resource initSchema = new ByteArrayResource(ByteStreams.toByteArray(inputStream));
+                resourceDatabasePopulator.addScript(new ClassPathResource("schema.sql"));
+                resourceDatabasePopulator.addScript(initSchema);
+            }
 
-    }
+        } catch (MalformedURLException exeption) {
+            exeption.printStackTrace();
+        } catch (IOException exeption) {
+            exeption.printStackTrace();
+        }
 
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(dataSource());
+        dataSourceInitializer.setDataSource(dataSource);
         dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
         return dataSourceInitializer;
     }
